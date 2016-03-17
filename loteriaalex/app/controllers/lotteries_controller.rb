d093f9157d9b9e1d6a12dfba5580160628625834
alexvkcr <alexvkcr@gmail.com>
2016-03-17 13:40:57 +0100

    Subida rutinaria

--- loteriaalex/app/controllers/lotteries_controller.rb
+++ loteriaalex/app/controllers/lotteries_controller.rb
@@ -1,4 +1,14 @@
 class LotteriesController < ApplicationController
   def index
+  	if session[:user_id].nil?
+  		@usuario = nil
+  	else
+  		@usuario = Loteries.find(session[:user_id])
+  	end
+  end
+  def login
+  	if not session[:user_id].nil?
+  		render index
+  	end
   end
 end
