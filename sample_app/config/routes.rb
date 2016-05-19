8957b4a2aa913093c79f7ab70eed5212e5b6e0c9
root <root@localhost.localdomain>
2016-05-19 09:41:36 +0200

    Puesta al día

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -17,9 +21,17 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
   resources :account_activations, only: [:edit]
-
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
